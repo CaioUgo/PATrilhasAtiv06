7487ff547617fabc07eb38e7d47922b289a2a84f
Caio <168545443+CaioUgo@users.noreply.github.com>
2025-02-06 09:09:46 -0300

    Atividade8-A.cs

---
+++ Atividade8-A.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace PATrilhasAtiv05
+{
+	class Program
+	{
+		public static void Main()
+		{				
+			for (int contador = 0; 	contador < 101; contador++){
+				
+				if (contador % 2 == 0) {
+					Console.Write(contador+ " ");
+				}
+			
+			}
+			Console.WriteLine( );
+			Console.WriteLine("\n\rPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
