6064823522e335667d526639c1502b63f9136c90
Caio <168545443+CaioUgo@users.noreply.github.com>
2025-02-09 18:40:10 -0300

    Atividade9-D.cs

---
+++ Atividade9-D.cs
@@ -0,0 +1,42 @@
+ 	using System;
+
+namespace PATrilhasAtiv05
+{
+	class Program
+	{
+		public static void Main()
+		{		 
+			
+			Console.WriteLine("Escolha o tamanho do vetor: ");
+			var tamanho = int.Parse(Console.ReadLine());
+			Console.WriteLine();
+			
+			int[] valores = new int[tamanho];			
+			int maiorvalor = int.MinValue;
+			
+			if (tamanho <= 0)
+			{
+				Console.WriteLine("Valor inválido!");
+				return;
+			}
+				
+			for (int i = 0; i < valores.Length; i++)
+			{
+				
+				Console.WriteLine("Digite o {0}º valor do vetor: ", i + 1);
+				valores[i] = int.Parse(Console.ReadLine());
+			
+				if (valores[i] > maiorvalor)
+				{
+					maiorvalor = valores[i];
+				}
+			}	
+			Console.Clear();
+			
+			Console.WriteLine("\n\rO maior núemro dessa sequência é : {0}", maiorvalor);	
+			Console.WriteLine();
+			Console.WriteLine("\n\rPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
