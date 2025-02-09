1c78493498c04de7046fad16aa5c882ca5e16292
Caio <168545443+CaioUgo@users.noreply.github.com>
2025-02-09 18:42:16 -0300

    Atividade9-E.cs

---
+++ Atividade9-E.cs
@@ -0,0 +1,42 @@
+ 	using System;
+
+namespace PATrilhasAtiv05
+{
+	class Program
+	{
+		public static void Main()
+		{		 
+			
+			Console.WriteLine("Escolha o tamanho do vetor: ");
+			var tamanho = int.Parse(Console.ReadLine());
+			Console.WriteLine();
+			
+			int[] valores = new int[tamanho];			
+			int menorvalor = int.MaxValue;
+			
+			if (tamanho <= 0)
+			{
+				Console.WriteLine("Valor inválido!");
+				return;
+			}
+				
+			for (int i = 0; i < valores.Length; i++)
+			{
+				
+				Console.WriteLine("Digite o {0}º valor do vetor: ", i + 1);
+				valores[i] = int.Parse(Console.ReadLine());
+			
+				if (valores[i] < menorvalor)
+				{
+					menorvalor = valores[i];
+				}
+			}	
+			Console.Clear();
+			
+			Console.WriteLine("\n\rO menor valor dessa sequência é : {0}", menorvalor);	
+			Console.WriteLine();
+			Console.WriteLine("\n\rPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
